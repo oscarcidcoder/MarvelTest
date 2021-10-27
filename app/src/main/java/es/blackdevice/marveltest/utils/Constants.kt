bb62f6f2f4fa148ed5068a7579bc7365079ee503
oscarcid <oscid88@gmail.com>
2021-10-27 20:11:00 +0200

    Constants singleton object

---
+++ app/src/main/java/es/blackdevice/marveltest/utils/Constants.kt
@@ -0,0 +1,12 @@
+package es.blackdevice.marveltest.utils
+
+import es.blackdevice.marveltest.BuildConfig
+
+/**
+ * Created by ocid on 10/27/21.
+ */
+object Constants {
+    val API_BASE_URL = BuildConfig.BASE_URL_MARVEL
+    val PRIVATE_KEY = BuildConfig.PRIVATE_KEY
+    val PUBLIC_KEY = BuildConfig.PUBLIC_KEY
+}
