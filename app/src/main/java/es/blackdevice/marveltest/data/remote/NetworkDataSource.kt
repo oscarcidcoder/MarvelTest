60dee87a2851ec70bf8db0f576cee71f7c488781
oscarcid <oscid88@gmail.com>
2021-10-28 23:00:34 +0200

    Mapper to list characters

--- app/src/main/java/es/blackdevice/marveltest/data/remote/NetworkDataSource.kt
+++ app/src/main/java/es/blackdevice/marveltest/data/remote/NetworkDataSource.kt
@@ -1,5 +1,8 @@
 package es.blackdevice.marveltest.data.remote
 
+import es.blackdevice.marveltest.utils.Result
+import es.blackdevice.marveltest.utils.convertIfSuccess
+
 /**
  * Created by ocid on 10/28/21.
  */
@@ -8,7 +11,10 @@ class NetworkDataSource(private val api: CharacterApi) : BaseDataSource() {
     suspend fun getCharacters(offset: Int) = api.getCharacters(offset = offset)
 
     suspend fun getCharacter(characterId: Int) = resolveResponse {
-        api.getCharacter(characterId)
-    }
+            api.getCharacter(characterId)
+        }.convertIfSuccess {
+            it.data.results
+        }
+
 
 }
